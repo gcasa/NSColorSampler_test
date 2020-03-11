4d8bf692d27070bb44085dcba265b37e97a5490c
Gregory Casamento <greg.casamento@gmail.com>
2020-03-11 18:03:22 -0400

    Initial Commit

---
+++ NSColorSampler_test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NSColorSampler_test
+//
+//  Created by Gregory Casamento on 3/11/20.
+//  Copyright © 2020 Open Logic. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ NSColorSampler_test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NSColorSampler_test
+//
+//  Created by Gregory Casamento on 3/11/20.
+//  Copyright © 2020 Open Logic. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
